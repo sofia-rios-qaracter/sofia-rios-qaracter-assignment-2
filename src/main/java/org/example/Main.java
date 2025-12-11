adb4addd7c88624b12e0418793bb282b49c0e4b1
Sofia <sofia.rios@qaracter.com>
2025-12-11 12:54:38 +0000

    Añadido selector de ejercicio en main
    Se ha añadido un selector del ejercicio en el main para poder ejecutar
    ambos ejercicios sin tener que modificar codigo.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //ReverseAndSearchArray.ReverseAndSearchArray();
-        StringBuilderManipulation.StringBuilderManipulation();
+        int selector = 0;
+        Scanner sc = new Scanner(System.in);
+        String mensaje = "Selecciona el ejercicio al que quieres acceder:\n1. Reverse And Search Array\n2. StringBuilderManipulation";
+
+
+        selector = ReverseAndSearchArray.readInt(sc, mensaje, "Debes de dar un numero entero");
+
+        if(selector == 1){
+            ReverseAndSearchArray.ReverseAndSearchArray();
+        } else if(selector == 2){
+            StringBuilderManipulation.StringBuilderManipulation();
+        } else{
+            System.out.println("No has seleccionado ninguna opción");
+        }
+
+        sc.close();
     }
 }
